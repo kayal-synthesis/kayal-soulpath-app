b69caf4d2897ca28f26184e6c7dec35bfb18789d
Kayal-Synthesis-Engine <samueges@gmail.com>
2026-06-11 16:18:23 +0800

    Remove duplicate dynamic exports

--- app/agency/chat/page.tsx
+++ app/agency/chat/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 export const dynamic = 'force-dynamic'
-export const dynamic = 'force-dynamic'
 
 import { useState, useRef, useEffect } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
